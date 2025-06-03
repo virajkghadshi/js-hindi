5393eca49ed7157380b51dd158913e05e9d864ea
virajkghadshi <virajghadshi001@gmail.com>
2025-06-03 13:47:14 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountID = 1234321
+let accountEmail = "virajghadshi001@gmail.com"
+var accountPassword = "12345"
+accountCity = "mumbai"
+let accountState;
+// accountID = 1
+
+console.log(accountID);
+
+accountEmail = "coder.com"
+accountPassword = "1212121"
+accountCity = "delhi"
+
+
+/*
+perfer not to use var
+because issue of block scope & function scope
+*/
+
+console.table([accountID,accountEmail,accountPassword,accountCity, accountState])
+
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert (3+3) //we are using nodejs, not browser
+
+console.log (3
+    +
+    3)  //not to write code like this
+ console.log("viraj")
+
+ let name = "viraj"
+ let age = 25
+ let isLoggedIn = false
+
+ //number => 2 to power 53
+ //bigint
+ //string => ""
+ //boolean => true/flase
+ //null => standalone value
+ //undefined =>
+    //symbol =>unique
+
+ //object
+
+ console.log(typeof undefined); //undefined
+ console.log(type of null); //object
